f92aa4285c3606088634b04671fb868f3a1af660
donat19 <96787147+donat19@users.noreply.github.com>
2024-11-24 13:47:16 -0330

    1.1

---
+++ python/IPO Logic.py
@@ -0,0 +1,37 @@
+# Description: 
+# Author: 
+# Date(s): 
+
+
+# Define required libraries.
+
+
+
+# Define program constants.
+
+
+
+# Define program functions.
+
+
+
+# Main program starts here.
+while True:
+    
+    # Gather user inputs.
+
+
+
+    # Perform required calculations.
+
+
+
+    # Display results
+
+
+
+    # Write the values to a data file for storage.
+
+
+
+# Any housekeeping duties at the end of the program.
